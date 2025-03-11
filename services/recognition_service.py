dc219cdad9f4cab6ddae90bfd7062ef58ec323f8
EnzoPENISSON <enzopenisson25@orange.fr>
2025-03-11 17:36:56 +0100

    Ajout de la method ocr_with_boxes

--- services/recognition_service.py
+++ services/recognition_service.py
@@ -13,3 +13,24 @@ class recognition_service:
         self.temp_image_path = "temp_page.png"
         pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
         self.selected_boxes = []
+
+    def save_temp_image(self):
+        """Save the current page as a temporary image."""
+        self.current_page.save(self.temp_image_path)
+
+    def extract_text_from_box(self, box):
+        """Extract text from a specific box in the image."""
+        cropped = self.current_page.crop(box)
+        return pytesseract.image_to_string(cropped)
+
+    def ocr_with_boxes(self, keyword):
+        """Perform OCR and locate bounding boxes for a specific keyword."""
+        ocr_data = pytesseract.image_to_data(self.current_page, output_type=pytesseract.Output.DICT)
+        found_boxes = []
+
+        for i, word in enumerate(ocr_data['text']):
+            if keyword.lower() in word.lower():
+                x, y, w, h = (ocr_data['left'][i], ocr_data['top'][i],
+                              ocr_data['width'][i], ocr_data['height'][i])
+                found_boxes.append((x, y, x + w, y + h))
+        return found_boxes
